c9576946cd7b75c9def77791d8a8bb077bd925cc
Ernesto Wetimane <wetimaneernestovasco@gmail.com>
2025-05-21 07:14:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+//Efeito Scrool para aparte de intro do meu site
+ ScrollReveal({
+     reset: true ,
+     distance:'80px',
+     duration: 2700,
+     delay: 200
+
+ });
+
+ ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
+ ScrollReveal().reveal('.home-img, .serviços-container, .projetos-caixa, .contacto form', { origin:'bootom' });
+  ScrollReveal().reveal('.home-content h1, .about-img', { origin:'left' });
+  ScrollReveal().reveal('.home-content p, .about-con', { origin:'right' });
+
+//Excrita js
+const typed = new Typed('.multiple-text',{
+    strings: ['Desenvolvedor Front-end', 'Desenvolvedor Front-end', 'Ernesto Vasco Wetimane '],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+//
