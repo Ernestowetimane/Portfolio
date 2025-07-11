9a27ffb043d245898851c7b9549787de369b31e2
Ernestowetimane <wetimaneernestovasco@gmail.com>
2025-07-11 11:18:41 +0100

    iniciao algo nvo

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+    document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const nome = document.querySelector('input[placeholder="Nome"]').value;
+    const email = document.querySelector('input[placeholder="Insira o seu email"]').value;
+    const numero = document.querySelector('input[placeholder="Numero de celular"]').value;
+    const mensagem = document.querySelector('textarea').value;
+
+    // Formata a mensagem para o WhatsApp
+    const texto = `Nome: ${nome}%0AEmail: ${email}%0ANúmero: ${numero}%0AMensagem: ${mensagem}`;
+    const telefone = '258856752762'; // Seu número de Moçambique
+
+        window.open(`https://wa.me/${telefone}?text=${texto}`, '_blank');
+    })
 
 //Efeito Scrool para aparte de intro do meu site
  ScrollReveal({
@@ -9,8 +23,8 @@
  });
 
  ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
- ScrollReveal().reveal('.home-img, .serviços-container, .projetos-caixa, .contacto form', { origin:'bootom' });
-  ScrollReveal().reveal('.home-content h1, .about-img', { origin:'left' });
+ ScrollReveal().reveal('.home-img, .serviços-container, .projetos-caixa, .contacto form', { origin:'bottom' });
+  ScrollReveal().reveal('.home-content h1, .about-img', { origin:'bottom' });
   ScrollReveal().reveal('.home-content p, .about-con', { origin:'right' });
 
 //Excrita js
