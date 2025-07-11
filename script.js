a2264f7bafd81f8cdc8e9507496d6934b30a53bc
Ernestowetimane <wetimaneernestovasco@gmail.com>
2025-07-11 11:38:13 +0100

    Altercapes
    adicionai muirta

--- script.js
+++ script.js
@@ -9,13 +9,13 @@
  });
 
  ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
- ScrollReveal().reveal('.home-img, .serviços-container, .projetos-caixa, .contacto form', { origin:'bootom' });
-  ScrollReveal().reveal('.home-content h1, .about-img', { origin:'left' });
+ ScrollReveal().reveal('.home-img, .serviços-container, .projetos-caixa, .contacto form', { origin:'bottom' });
+  ScrollReveal().reveal('.home-content h1, .about-img', { origin:'bottom' });
   ScrollReveal().reveal('.home-content p, .about-con', { origin:'right' });
 
 //Excrita js
 const typed = new Typed('.multiple-text',{
-    strings: ['Desenvolvedor Front-end', 'Desenvolvedor Front-end', 'Ernesto Vasco Wetimane '],
+    strings: ['Desenvolvedor Front-end', 'Desenvolvedor Front-end',],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
